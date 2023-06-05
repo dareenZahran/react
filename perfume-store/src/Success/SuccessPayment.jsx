386d9b0cc00ddedde8c23d6ceb81bfba49038174
salsabeel-alsahory <salsabeel2018as@gmail.com>
2023-06-05 06:25:40 +0300

    successful pay

--- perfume-store/src/Success/SuccessPayment.jsx
+++ perfume-store/src/Success/SuccessPayment.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const SuccessPayment = () => {
   return (
     <div className="payment-success">
-      <h2>Payment Successful!</h2>
-      <p>Thank you for your payment.</p>
-      <p>Your payment has been processed successfully.</p>
+      <div className="center">
+      <img src="img/download.png" alt="Success" />
+      <h2>Payment Successful!</h2></div>
     </div>
   );
 };
